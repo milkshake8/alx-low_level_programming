edca469a8a9bb8729cea37f69c5d4c2a0bc460d4
Malick79 <diop79.malick@gmail.com>
2022-09-15 02:42:13 +0000

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - first 50 Fibonacci numbers, starting with 1 and 2
+ * followed by a new line
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (j % 2) == 0)
+		{
+			sum = sum + j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
