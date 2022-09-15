40522b74c8894dc6227dd1911714f45bf95946ec
Malick79 <diop79.malick@gmail.com>
2022-09-15 02:20:13 +0000

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,17 +7,25 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i  < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			k = i * j;
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-			if (k != 81)
+			if (j == 0)
+				_putchar(k + '0');
+			if (j != 0 && k < 10)
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
+				_putchar(k + '0');
+			} else if (k >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
 			}
 		}
 		_putchar('\n');
