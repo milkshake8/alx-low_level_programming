515cb6ec5adb174349f11e6b9aaff3af5c643444
Malick79 <diop79.malick@gmail.com>
2022-09-15 02:23:45 +0000

    Sum of two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - adds two integers and returns the result
+* @i : fisrt integer parameter
+* @j : second integer parameter
+* Return: return the value of the sum of the
+* two parameters.
+*/
+
+int add(int i, int j)
+{
+	return (i + j);
+}
