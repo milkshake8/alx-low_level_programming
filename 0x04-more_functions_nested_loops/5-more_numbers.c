bd415ba99534b775473cda4813d4af29d1556285
Malick79 <diop79.malick@gmail.com>
2022-09-16 20:49:16 +0000

    More numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 times the numbers, from 0 to 14,
+ * followed by a new line
+ */
+
+void more_numbers(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			_putchar(j);
+		}
+		putchar('\n');
+	}
+}
