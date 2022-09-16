b5622a2b115ba9cd57d77404575039362bc510f4
Malick79 <diop79.malick@gmail.com>
2022-09-16 19:20:41 +0000

    Function: print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ *
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i == 48; i < 58; i++)
+	{
+		_putchar(i + '0');
+	}
+	
+	putchar('\n');
+	
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 int _isupper(char c);
 int _isdigit(char c);
 int mul(int a, int b);
+void print_numbers(void);
