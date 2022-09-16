d76df046523e91955834e7871ae1cade604b55d8
Malick79 <diop79.malick@gmail.com>
2022-09-16 22:21:35 +0000

    Draw a diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,15 +9,15 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-		putchar('\n');
+		_putchar('\n');
 	else
 	{
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < i; j++)
-				putchar(' ');
-			putchar('\\');
-			putchar('\n');
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
 		}
 	}
 }
