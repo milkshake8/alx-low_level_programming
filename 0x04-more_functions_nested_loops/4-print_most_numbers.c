796cde6c0246f46a2b1fef47f1cae25de876b265
Malick79 <diop79.malick@gmail.com>
2022-09-16 20:24:25 +0000

    Print numbers escept 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9 excpet 2 and 4
+ * followed by a new line.
+ *
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i != 50 && i != 52)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,4 @@ int _isupper(char c);
 int _isdigit(char c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
