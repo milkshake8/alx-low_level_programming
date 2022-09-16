64bef8c4ab148cd967cbfded46cc7ddf7c2ac097
Malick79 <diop79.malick@gmail.com>
2022-09-16 23:04:45 +0000

    Draw a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,10 +8,15 @@ void print_square(int size)
 {
 	int i, j;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
+		_putchar('\n');
+	else
 	{
-		for (j = 0; j < size; j++)
-			putchar('#');
-		putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
