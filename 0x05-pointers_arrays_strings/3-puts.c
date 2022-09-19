6bd2a1a4d91c6f8116611798aa23e2a5f03983e1
Malick79 <diop79.malick@gmail.com>
2022-09-19 23:40:24 +0000

    Print string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,7 @@
  * _puts - prints a string, followed by a new line, to stdout.
  * @str: point to the address of the string to print out.
  */
-void _puts(char *str);
+void _puts(char *str)
 {
 	printf("%s\n", *str);
 }
