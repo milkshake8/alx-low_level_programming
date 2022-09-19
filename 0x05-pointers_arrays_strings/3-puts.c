c6b1913736665ce9c3588ccd44c0bce6a9f5ff1f
Malick79 <diop79.malick@gmail.com>
2022-09-19 23:39:24 +0000

    Print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout.
+ * @str: point to the address of the string to print out.
+ */
+void _puts(char *str);
+{
+	printf("%s\n", *str);
+}
