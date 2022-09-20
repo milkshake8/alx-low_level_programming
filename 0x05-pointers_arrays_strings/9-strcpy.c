ad2240e33e31a69d1ea2e14ed7291f299cadf214
Malick79 <diop79.malick@gmail.com>
2022-09-20 12:53:39 +0000

    Copy string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -15,9 +15,9 @@ char *_strcpy(char *dest, char *src)
 	int size = strlen(src);
 	int i;
 
-	for (i = 0; i < size; i++)
-	{
-			dest[i] = src[i];
-	}
+	for (i = 0; i < size && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for ( ; i < size; i++)
+		dest[i] = '\0';
 	return (dest);
 }
