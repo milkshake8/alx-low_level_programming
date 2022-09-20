7ea95cf4d002b28a98850259782c134078bea5ae
Malick79 <diop79.malick@gmail.com>
2022-09-20 01:00:54 +0000

    Reverse string 2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,11 +7,14 @@
  */
 void rev_string(char *s)
 {
-	int i;
+	int i, k;
 	int size = strlen(s);
-	char _s[size];
 
-	for (i = 0; i < size; i++)
-		_s[i] = s[(size - 1) - i];
-	s = _s;
+	for (i = 0; i < size / 2; i++)
+	{
+		k = s[i];
+		s[i] = s[(size - 1) - i];
+		s[(size - 1) - i] = k;
+	}
+
 }
