28e4b793ca36d070ca9f5c49cb186952629e3371
Malick79 <diop79.malick@gmail.com>
2022-09-20 12:21:05 +0000

    Print array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,13 @@
 void print_array(int *a, int n)
 {
 	int i;
-
-	for (i = 0; i < n; i++)
-		if (i != n - 1)
-			printf("%d, ", a[i]);
-		else
-			printf("%d\n", a[i]);
+	
+	if ( n > 0)
+	{
+		for (i = 0; i < n; i++)
+			if (i != n - 1)
+				printf("%d, ", a[i]);
+			else
+				printf("%d\n", a[i]);
+	}
 }
