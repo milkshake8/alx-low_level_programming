46133da11971b810d9b7c5d8befd7aa67e151382
Malick79 <diop79.malick@gmail.com>
2022-09-22 18:11:59 +0000

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,19 +10,10 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while (s1[i] != '\0' || s2[i] != '\0')
+	while (*s1 && (*s1 == *s2))
 	{
-		if (s1[i] > s2[i] && s1[i] != '\0' && s2[i] != '\0')
-			return (15);
-		else if (s1[i] < s2[i] && s1[i] != '\0' && s2[i] != '\0')
-			return (-15);
-		else if (s1[i] == '\0')
-			return (15);
-		else if (s2[i] == '\0')
-			return (-15);
-		i++;
+		s1++;
+		s2++;
 	}
-	return (0);
+	return (*s1 - *s2);
 }
