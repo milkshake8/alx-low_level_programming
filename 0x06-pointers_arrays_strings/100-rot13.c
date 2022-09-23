3d3a1858155b9404f27a871f4a63679de517357d
Malick79 <diop79.malick@gmail.com>
2022-09-23 23:38:10 +0000

    Encode to Rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string using rot13.
+ * @str: point to the string to encode.
+ * Return: a pointer to the encoded string.
+ */
+
+char *rot13(char *str)
+{
+	int count1 = 0, count2 = 0;
+	char letters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char replace[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count2 = 0;
+		while (letters[count2] != '\0')
+		{
+			if (str[count] == letters[count2])
+			{
+				str[count] = replace[count2];
+			}
+			count2++;
+		}
+		count++;
+	}
+	return (str);
+}
