e267ff81016730a332183febe8fd7f8581d9c466
Malick79 <diop79.malick@gmail.com>
2022-09-24 00:54:40 +0000

    Print the buffer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,19 +1,34 @@
 #include "main.h"
 
 /**
- * print_number - function that prints an integer.
+ * print_number - prints an integer.
  * @n: the integer to print
  */
 void print_number(int n)
 {
+	unsigned int m, d, count;
+
 	if (n < 0)
 	{
-		n = -n;
-		_putchar('-');
+		_putchar(45);
+		m = n * -1;
 	}
-	if (n / 10)
+	else
 	{
-		print_number(n / 10);
+		m = n;
 	}
-	_putchar(n % 10 + '0');
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -11,22 +11,22 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j;
+	int count, count2;
 
-	while (n1[i] != '\0')
-		i++;
-	while (n2[j] != '\0')
-		i2++;
+	while (n1[count] != '\0')
+		count++;
+	while (n2[count2] != '\0')
+		count2++;
 
 	*r = *(r + size_r);
-	while (n1[i] > 0 || n1[j] > 0)
+	while (n1[count] > 0 || n1[count2] > 0)
 	{
-		if (n1[i] + n2[j] > 0)
-			*r = n1[i - 1] + n2[j - 1] + 1;
+		if (n1[count] + n2[count2] > 0)
+			*r = n1[count - 1] + n2[count2 - 1] + 1;
 		else
-			*r = n1[i] + n2[j];
-		i--;
-		j--;
+			*r = n1[count] + n2[count2];
+		count--;
+		count2--;
 	}
 	return (r);
 }
