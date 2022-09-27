a4c7eeacebc28e72c1b76b15c292e32e4ff2530e
Malick79 <diop79.malick@gmail.com>
2022-09-27 21:08:04 +0000

    Locate a substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,30 +8,24 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int i = 0, size = 0, word_size;
+	unsigned int i = 0, j = 0;
 
-	while (needle[i] != '\0')
+	while (haystack[i])
 	{
-		size++;
-		i++;
-	}
-	while (*haystack != '\0')
-	{
-		word_size = 0;
-		i = 0;
-		while (needle[i] != '\0')
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (*(haystack + i) == needle[i])
-			{
-				word_size++;
-			}
+			if (haystack[i + j] == needle[j])
+				j++;
 			else
 				break;
-			i++;
 		}
-		if (word_size == size)
-			return (needle);
-		haystack++;
+		if (needle[j])
+		{
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
 	}
 	return (0);
 }
