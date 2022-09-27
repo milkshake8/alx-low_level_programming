1906d835e47f61cc2bbd3a0b6aaa14aab348a293
Malick79 <diop79.malick@gmail.com>
2022-09-27 20:53:35 +0000

    Print sum of two diagonals

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: the string in which to search.
+ * @needle: the sub-chain to search in the main chain.
+ * Return: substring or null
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, size = 0, word_size;
+
+	while (needle[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+	while (*haystack != '\0')
+	{
+		word_size = 0;
+		i = 0;
+		while (needle[i] != '\0')
+		{
+			if (*(haystack + i) == needle[i])
+			{
+				word_size++;
+			}
+			else
+				break;
+			i++;
+		}
+		if (word_size == size)
+			return (needle);
+		haystack++;
+	}
+	return (NULL);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sums of the two diagonals of a square matrix
+ * @a: pointer to start of matrix
+ * @size: size of matrix row
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size * size; i++)
+	{
+		if (i % (size + 1) == 0)
+			sum1 += a[i];
+		if ((i % (size - 1) == 0) && i != 0 && i != (size * size) - 1)
+			sum2 += a[i];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
