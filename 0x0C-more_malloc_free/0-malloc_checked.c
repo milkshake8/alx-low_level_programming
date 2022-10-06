01f945be7b0dcf82936d77a0a1f3b05dd2b93a87
Malick79 <diop79.malick@gmail.com>
2022-10-06 23:00:50 +0000

    Check malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to allocate
+ *
+ * Return: pointer to allocated memory or normal process termination if error
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+In this directory we are going to learn deeply how to allocate memory with the function malloc
