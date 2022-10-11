2f8c24c7929484a5644be2da8e0761fbbaad295b
Malick79 <diop79.malick@gmail.com>
2022-10-11 02:34:28 +0000

    Initialize a dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,16 +9,7 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
-	{
-		if (d->name != NULL)
-			d->name = name;
-		else
-			exit(98);
-		d->age = age;
-		if (d->owner != NULL)
-			d->owner = owner;
-		else
-			exit(98);
-	}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
