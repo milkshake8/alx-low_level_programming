9312b2ea0ccf626f5da1eccb21499e0f3d148040
Malick79 <diop79.malick@gmail.com>
2022-10-11 01:25:20 +0000

    Dog structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef _DOG_H_
 #define _DOG_H_
+
+/**
+ * struct dog - structure of a dog type
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the owner.
+ *
+ * Description: A structure created for descripting a dog.
+ */
 struct dog
 {
 	char *name;
