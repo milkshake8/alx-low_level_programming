cfeb1ff43a99b77f69aab6fdfb6d4b26b62bf063
Malick79 <diop79.malick@gmail.com>
2022-10-11 23:17:23 +0000

    Add a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -8,15 +8,37 @@
  * @owner: struct parameter owner
  * Return: returns pointer to buffer of datatype dog
  */
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	dog_t *doggy;
+unsigned int nl, ol, i;
+	dog_t *dog;
 
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+	if (name == NULL || owner == NULL)
 		return (NULL);
-	doggy->name = name;
-	doggy->age = age;
-	doggy->owner = owner;
-	return (doggy);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
+	for (nl = 0; name[nl]; nl++)
+		;
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
 }
