2ea56d5c6d990e17f3e0f6d4344b493912e8c70a
Malick79 <diop79.malick@gmail.com>
2022-10-27 13:51:48 +0000

    binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,20 +12,22 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, size = 0, numb = 0;
 
-	if (b == NULL)
-		return (0);
-	while (b[i] != '\0')
+	if (b)
 	{
-		size++;
-		i++;
+		while (b[i] != '\0')
+		{
+			size++;
+			i++;
+		}
+		i = 0;
+		while (b[i] != '\0')
+		{
+			if (b[i] != '0' && b[i] != '1')
+				return (0);
+			numb += (b[i] - 48) * pow(2, (size - i - 1));
+			i++;
+		}
+		return (numb);
 	}
-	i = 0;
-	while (b[i] != '\0')
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		numb += (b[i] - 48) * pow(2, (size - i - 1));
-		i++;
-	}
-	return (numb);
+	return (0);
 }
